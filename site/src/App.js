60778f34f2d4779fcadf6c491fd196e770a33339
dbhosale-fti <dbhosale@ftilab.com>
2020-12-27 23:58:45 +0530

    Update

--- site/src/App.js
+++ site/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>This site deployed by using CI/CD</h1>
+      <h1>This site deployed by using CI/CD at branch PREVIEW</h1>
     </div>
   );
 }
